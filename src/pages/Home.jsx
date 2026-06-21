46cca34d92d1fc5248c3e1add964cf0f9190a4a8
marco <drflight7@gmail.com>
2026-06-21 16:29:59 +0800

    test: UI update for deployment check

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -478,7 +478,7 @@ const Home = () => {
   const features = [
     {
       icon: FileText,
-      title: "Submit Complaints",
+      title: "Submit Complaint(s)",
       description:
         "Easily submit your concerns through our streamlined feedback form. Anonymous submissions are welcome.",
       color: "bg-maroon-800",
